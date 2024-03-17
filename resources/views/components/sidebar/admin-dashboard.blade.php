ba9d94227e942e3eb16c230d0f3ffdd062fb6460
liamjosefk <liam.dmgurus@gmail.com>
2024-03-16 23:53:51 -0700

    new build

--- resources/views/components/sidebar/admin-dashboard.blade.php
+++
@@ -1,5 +0,0 @@
-<li class="nav-item {{ Request::is('admin/dashboard*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{route('admin.index')}}">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span></a>
-</li>
