fee057efc9e4c6b0e7808f077ebc4bcf91df933c
liamjosefk <liam.dmgurus@gmail.com>
2024-03-20 22:17:19 -0700

    home  video update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -206,13 +206,15 @@
         <!-- /Videos -->
 
         <!-- Email -->
-        <div class="row text-center mb-5">
-            <div class="col-12">
-                <h3 class="raleway-bold text-black text-3rem mt-5">
-                    <a href="mailto:dojump@dojump.org">dojump@dojump.org</a>
-                </h3>
-            </div>
-        </div>
+       <div class="container">
+           <div class="row text-center mb-5">
+               <div class="col-12">
+                   <h3 class="raleway-bold text-black text-3rem mt-5">
+                       <a href="mailto:dojump@dojump.org">dojump@dojump.org</a>
+                   </h3>
+               </div>
+           </div>
+       </div>
         <!-- /Email -->
 
     </div>
