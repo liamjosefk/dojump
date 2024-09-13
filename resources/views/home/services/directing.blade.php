4ff1b28650ea4b92a29b91c6ab4ea34aec37222c
liamjosefk <liam.dmgurus@gmail.com>
2024-09-12 17:41:49 -0700

    services/directing

--- resources/views/home/services/directing.blade.php
+++ resources/views/home/services/directing.blade.php
@@ -50,6 +50,84 @@
         <!-- /Above the Fold -->
 
 
+        <!-- Directing Content -->
+        <div class="container mt-3">
+            <div class="row text-center">
+                <div class="col-12">
+                    <h2 class="text-primary mt-3">
+                        Directing your show. Facilitating the creation of your original work.
+                    </h2>
+                </div>
+                <div class="col-sm-8 offset-sm-2">
+                    <p>
+                        Lane brings the experience of four decades of creating ensemble with companies from diverse performance backgrounds. Her unique directing approach can help you bring your creative process into focus, create a cast deeply connected to each other, to the truth of their material and to communicating that truth while connecting with , and entertaining their audience.The powerful and gradient methods work to propel performers and students past fixed ideas and limitations.
+                    </p>
+                    <a href="#" class="btn btn-primary mt-4">
+                        We Can Help
+                    </a>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col-sm-10 offset-sm-1">
+                    <div class="card shadow mt-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <img src="{{asset('storage/images/ahhh.jpg')}}" class="d-block img-fluid w-100" alt="...">
+                                </div>
+                                <div class="col-sm-8">
+                                    <h3>
+                                        Mentoring, Workshops, & Design
+                                    </h3>
+                                    <p>
+                                        Custom Aerial Dance workshops Choreography, Rigging and Lighting Design. Ensemble building workshops, Original material generating workshops and mentoring.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card shadow mt-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-8">
+                                    <h3 class="text-sm-right">
+                                        Directing Your Circus Act
+                                    </h3>
+                                    <p class="text-sm-right">
+                                        Let us help you hone the unique talents and performance qualities that inspire you to shine. We can create or fine tune your choreography, discover and realize your theatrical concept, and enhance your musicality and technique.
+                                    </p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <img src="{{asset('storage/images/tiger-lilly.jpg')}}" class="d-block img-fluid w-100" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card shadow mt-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <img src="{{asset('storage/images/dizzy-heights.jpg')}}" class="d-block img-fluid w-100" alt="...">
+                                </div>
+                                <div class="col-sm-8">
+                                    <h3>
+                                        Show Doctor
+                                    </h3>
+                                    <p>
+                                        We can help you integrate multi media into your performance, create a scene that is outside your normal area of expertise, add spectacle , circus or aerial work to your show…
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /Directing Content -->
+
+
 
         <!-- Email -->
         <div class="container">
